467a5485cd294fa4e2876db9aa52d492355dd606
Humberto Pereira <humberto_pereira@live.com>
2023-08-15 16:37:26 +0000

    Final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//api url, contain instructions for implementation: https://ci-jshint.herokuapp.com/
+
 const API_kEY = 'cfOdOnf7Hnn2KB7JJI9A8zRhQQg';
 const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
@@ -5,8 +7,26 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === 'options') {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete('options');
+    form.append('options', optArray.join()); //optArray.join() append a list of strings separated by comma as the api instructions
+
+    return form
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
+
+    for (let entry of form.entries()) {
+        console.log(entry); // log to the console processOption() array
+    }
 
     const response = await fetch(API_URL, {
         method: "POST", // post request to the api
@@ -21,6 +41,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -54,6 +75,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -63,6 +85,19 @@ function displayStatus(data) {
     let heading = 'API Key Status';
     let results = `<div>Your key is valid until:</div>`;
     results += `<div class='key-status'>${data.expiry}</div>`;
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
+}
+
+function displayException(data) { // show messages with errors when they occur.
+
+    let heading = 'An Exception Occurred';
+    results = `<div>The API returned status code:${data.status_code}</div>`;//all the: ${data.-?-} is mentioned in the api instructions.
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`
+    results += `<div>Error text: <strong>${data.error}</strong></div>`
 
     document.getElementById('resultsModalTitle').innerText = heading;
     document.getElementById('results-content').innerHTML = results;
